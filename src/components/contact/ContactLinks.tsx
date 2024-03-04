df54e8a9f888a164107ae4406f63e4024cb326e1
Javier García <javigcpgr@gmail.com>
2024-03-04 20:36:04 +0100

    fix(ContactLinks.tsx): padding, space

--- src/components/contact/ContactLinks.tsx
+++ src/components/contact/ContactLinks.tsx
@@ -4,16 +4,18 @@ import { contactData } from './contact.data';
 
 const ContactLinks = () => {
     return (
-        <nav className="contact-links">
-            {contactData.map((contact) => (
-                <div key={contact.id} className="contact-item">
-                    <a href={contact.info} target="_blank" rel="noopener noreferrer">
-                        {contact.icon}
-                        <span className='contact-name'>{contact.platform}</span>
-                    </a>
-                </div>
-            ))}
-        </nav>
+        <>
+            <nav className="contact-links">
+                {contactData.map((contact) => (
+                    <div key={contact.id} className="contact-item">
+                        <a href={contact.info} target="_blank" rel="noopener noreferrer">
+                            {contact.icon}
+                        </a>
+                    </div>
+                ))}
+            </nav>
+            <div className='space-container'></div>
+        </>
     );
 };
 

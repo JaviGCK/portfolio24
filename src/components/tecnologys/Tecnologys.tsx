27f24ad2c719166aa25c0e11bbdf81097d9e3d34
Javier García <javigcpgr@gmail.com>
2024-03-10 19:54:38 +0100

    feat(Tecnologys):group of tech & animation & logic for different grid

--- src/components/tecnologys/Tecnologys.tsx
+++ src/components/tecnologys/Tecnologys.tsx
@@ -8,6 +8,7 @@ export const Tecnologys = () => {
     const tecnologysRef = useRef<HTMLDivElement>(null);
     const [isVisible, setIsVisible] = useState(false);
     const [visibleItems, setVisibleItems] = useState<string[]>([]);
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth); // Obtener el ancho del viewport
 
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
@@ -32,40 +33,56 @@ export const Tecnologys = () => {
     }, []);
 
     useEffect(() => {
-        const timer1 = setTimeout(() => {
-            setVisibleItems(['html', 'css', 'js']);
-        }, 500);
+        const setTechGroups = () => {
+            setVisibleItems([]);
+            if (viewportWidth >= 1285) { // grid 5 columns
+                const timer1 = setTimeout(() => setVisibleItems(['html']), 375);
+                const timer2 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'ts', 'css']), 750);
+                const timer3 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'auth0', 'react', 'bootstrap']), 1125);
+                const timer4 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'github', 'cloudinary', 'next.js', 'tailwind']), 1500);
+                const timer5 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'figma', 'git', 'vercel', 'mongodb', 'js']), 1875);
+                const timer6 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'express', 'netlify', 'postgress']), 2250);
+                const timer7 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'vscode', 'jenkin']), 2625);
+                const timer8 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'postman']), 3000);
+                return [timer1, timer2, timer3, timer4, timer5, timer6, timer7, timer8];
+            } else if (viewportWidth <= 1284 && viewportWidth >= 1051) { // grid 4 columns
+                const timer1 = setTimeout(() => setVisibleItems(['html']), 375);
+                const timer2 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'css', 'js']), 750);
+                const timer3 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'mongodb', 'ts', 'bootstrap']), 1125);
+                const timer4 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'vercel', 'postgress', 'react', 'tailwind']), 1500);
+                const timer5 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'git', 'netlify', 'auth0', 'next.js']), 1875);
+                const timer6 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'figma', 'express', 'jenkin', 'cloudinary']), 2250);
+                const timer7 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'vscode', 'github']), 2625);
+                const timer8 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'postman']), 3000);
+                return [timer1, timer2, timer3, timer4, timer5, timer6, timer7, timer8];
+            } else { // grid 3 columns
+                const timer1 = setTimeout(() => setVisibleItems(['html', 'css', 'bootstrap']), 428);
+                const timer2 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'tailwind', 'js', 'ts']), 856);
+                const timer3 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'react', 'next.js', 'mongodb']), 1284);
+                const timer4 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'postgress', 'auth0', 'cloudinary']), 1712);
+                const timer5 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'vercel', 'netlify', 'jenkin']), 2140);
+                const timer6 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'github', 'git', 'express']), 2568);
+                const timer7 = setTimeout(() => setVisibleItems(prevItems => [...prevItems, 'vscode', 'figma', 'postman']), 2996);
+                return [timer1, timer2, timer3, timer4, timer5, timer6, timer7];
+            }
+        };
 
-        const timer2 = setTimeout(() => {
-            setVisibleItems(prevItems => [...prevItems, 'mongodb', 'ts', 'bootstrap']);
-        }, 1000);
 
-        const timer3 = setTimeout(() => {
-            setVisibleItems(prevItems => [...prevItems, 'vercel', 'postgress', 'react', 'tailwind']);
-        }, 1500);
-
-        const timer4 = setTimeout(() => {
-            setVisibleItems(prevItems => [...prevItems, 'git', 'netlify', 'auth0', 'next.js']);
-        }, 2000);
-
-        const timer5 = setTimeout(() => {
-            setVisibleItems(prevItems => [...prevItems, 'figma', 'express', 'jenkin', 'cloudinary']);
-        }, 2500);
-
-        const timer6 = setTimeout(() => {
-            setVisibleItems(prevItems => [...prevItems, 'vscode', 'github', 'postman']);
-        }, 3000);
-
-        // Agregar más grupos con sus respectivos temporizadores según el patrón
+        const timerSet = setTechGroups();
 
         return () => {
-            clearTimeout(timer1);
-            clearTimeout(timer2);
-            clearTimeout(timer3);
-            clearTimeout(timer4);
-            clearTimeout(timer5);
-            clearTimeout(timer6);
-            // Limpiar temporizadores adicionales si es necesario
+            timerSet.forEach(timer => clearTimeout(timer));
+        };
+    }, [viewportWidth]); // Dependencia del ancho del viewport
+
+    const handleResize = () => {
+        setViewportWidth(window.innerWidth); // Actualizar el ancho del viewport cuando cambie el tamaño de la ventana
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize); // Agregar listener para cambios en el tamaño de la ventana
+        return () => {
+            window.removeEventListener('resize', handleResize); // Remover el listener al desmontar el componente
         };
     }, []);
 
